4d8a552ec20ba4f19c8907fa41450b2534e0d4fc
Muhammed Machrouh <th3justhacker@gmail.com>
2024-04-26 12:08:29 +0100

    Add new files and tools for MISP search and CVE search

--- agents/investigator.py
+++ agents/investigator.py
@@ -23,8 +23,9 @@ llama3 = Ollama(model="llama3", base_url=os.getenv('OLLAMA_HOST'), temperature=0
 cve_search_tool = CVESearchTool().cvesearch
 misp_search_tool =  MispTool().search
 misp_search_by_date_tool = MispTool().search_by_date
+misp_search_by_event_id_tool = MispTool().search_by_event_id
 
-tools = [cve_search_tool, misp_search_tool, misp_search_by_date_tool]
+tools = [cve_search_tool, misp_search_tool, misp_search_by_date_tool, misp_search_by_event_id_tool]
 
 # conversational agent memory
 memory = ConversationBufferWindowMemory(
@@ -50,7 +51,10 @@ conversational_agent = initialize_agent(
 )
 
 # conversational_agent.agent.llm_chain.prompt.messages[0].prompt.template = """
-# 'Respond to the human as helpfully and accurately as possible. You have access to the following tools:\n\n\n\nUse a json blob to specify a tool by providing an action key (tool name) and an action_input key (tool input).\n\nValid "action" values: "Final Answer" or \n\nProvide only ONE action per $JSON_BLOB, as shown:\n\n```\n{{\n  "action": $TOOL_NAME,\n  "action_input": $INPUT\n}}\n```\n\nFollow this format:\n\nQuestion: input question to answer\nThought: consider previous and subsequent steps\nAction:\n```\n$JSON_BLOB\n```\nObservation: action result\n... (repeat Thought/Action/Observation N times)\nThought: I know what to respond\nAction:\n```\n{{\n  "action": "Final Answer",\n  "action_input": "Final response to human"\n}}\n```\n\nBegin! Reminder to ALWAYS respond with a valid json blob of a single action. Use tools if necessary. Respond directly if appropriate. Format is Action:```$JSON_BLOB```then Observation:.\nThought:'
+# 'Respond to the human as helpfully and accurately as possible. 
+# You should use the tools available to you to help answer the question.
+# Your final answer should be technical, well explained, and accurate.
+# You have access to the following tools:\n\n\n\nUse a json blob to specify a tool by providing an action key (tool name) and an action_input key (tool input).\n\nValid "action" values: "Final Answer" or \n\nProvide only ONE action per $JSON_BLOB, as shown:\n\n```\n{{\n  "action": $TOOL_NAME,\n  "action_input": $INPUT\n}}\n```\n\nFollow this format:\n\nQuestion: input question to answer\nThought: consider previous and subsequent steps\nAction:\n```\n$JSON_BLOB\n```\nObservation: action result\n... (repeat Thought/Action/Observation N times)\nThought: I know what to respond\nAction:\n```\n{{\n  "action": "Final Answer",\n  "action_input": "Final response to human"\n}}\n```\n\nBegin! Reminder to ALWAYS respond with a valid json blob of a single action. Use tools if necessary. Respond directly if appropriate. Format is Action:```$JSON_BLOB```then Observation:.\nThought:'
 # """
 
 def invoke(input_text):
--- tools/misp_tool.py
+++ tools/misp_tool.py
@@ -19,19 +19,19 @@ misp = PyMISP(url=URL, key=KEY, ssl=verify_cert)
 class MispTool():
     @tool("MISP search Tool by keyword")
     def search(keyword: str):
-      """Useful tool to search for an indicator of compromise or an security event
+      """Useful tool to search for an indicator of compromise or an security event by keyword
       Parameters:
       - keyword: The keyword to search for
       Returns:
       - A list of events that match the keyword
       """
 
-      events = misp.search(controller='attributes', value=keyword, limit=1, metadata=True, include_event_tags=False, include_context=False, return_format='json', sg_reference_only=True)
+      events = misp.search(controller='attributes', value=keyword, limit=5, metadata=True, include_event_tags=False, include_context=False, return_format='json', sg_reference_only=True)
       results = """Answer user question using these search results:\n\n"""
       return results + str(events)
     
     @tool("MISP search Tool by date")
-    def search_by_date(date_from: str = None, date_to: str = None, metadata=True):
+    def search_by_date(date_from: str = None, date_to: str = None):
       """Useful tool to retrieve events that match a specific date or date range, use this if you know the date of the event
       Parameters:
       - date_from: The start date of the event
@@ -42,7 +42,18 @@ class MispTool():
       - A list of events that match the date or date range
       """
 
-      events = misp.search(date_from=date_from, date_to=date_to, limit=5)
+      events = misp.search(controller='attributes',date_from=date_from, date_to=date_to, limit=5)
       return events
-    
+
+    @tool("MISP search Tool by event_id")
+    def search_by_event_id(event_id: str | int):
+      """Useful tool to retrieve events by their ID, use this if you know the ID of the event.
+      Parameters:
+      - event_id: The ID of the event
+      Returns:
+      - A list of events that match the event ID
+      """
+
+      events = misp.search(controller='attributes', eventid=event_id, limit=1)
+      return events
     
